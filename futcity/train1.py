74009406c6b70414fd1fb8c0f36970e4b729df2e
yasith46 <yasithudana3@gmail.com>
2024-02-24 14:09:50 +0530

    renamed test1.py as train1.py

--- futcity/train1.py
+++ futcity/train1.py
@@ -7,6 +7,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_percentage_error
+import joblib
 
 
 #Loading data and removing high cardinality categorical columns
@@ -15,7 +16,7 @@ data = pd.read_csv('futcity/futuristic_city_traffic.csv')
 y = data['Traffic Density']
 X = data.drop(['Traffic Density', 'Energy Consumption'], axis=1)  # Check for improvements after removing economic status
 
-X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.3, test_size=0.02)
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2)
 
 cat_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique()<10 and X_train_full[cname].dtype == 'object']
 num_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
@@ -55,8 +56,10 @@ my_pipeline = Pipeline(steps = [
 
 
 my_pipeline.fit(X_train, y_train)
-print('Predicting...')
-preds = my_pipeline.predict(X_valid)
+#print('Predicting...')
+#preds = my_pipeline.predict(X_valid)
 
-score = mean_absolute_percentage_error(y_valid, preds)
-print('MAE: ', score)
+#score = mean_absolute_percentage_error(y_valid, preds)
+#print('MAE: ', score)
+
+joblib.dump(model, 'trained_model1.joblib')
